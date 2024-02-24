39657846994b25d5a6778ff965e5a0378cdd889e
Samee Rauf <sameerauf1@gmail.com>
2024-02-24 12:15:59 -0800

    added calendar(but spacing wierd

---
+++ Calendar/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Calendar from "./components/Calendar"
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Itineary for 7 Days in Dallas ✌️</h1> 
+      <h2> Welcome to Dallas, Samee! Check out this calendar to get to know the city and see all the sights during your stay</h2>
+      <Calendar/>
+    </div>
+  )
+}
+
+export default App
---
+++ Calendar/src/components/Calendar.jsx
@@ -0,0 +1,130 @@
+import React from "react";
+import Event from './Event'
+
+
+const Calendar = () => {
+  return (
+    <div className="Calendar">
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Sunday</th>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+            <th>Saturday</th>
+          </tr>
+        </thead>
+        <tbody>
+          
+          <tr>
+          <td className="time">8 am</td>
+            <Event />
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+          <td className="time">9 am</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>10 am</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>11 am</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>12 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>1 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>2 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>3 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>4 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>5 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          {/* Repeat similar rows for other times */}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Calendar;
