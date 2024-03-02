ab0ba6d815f67c69c191b5cf719cdf944e946ef0
Samee Rauf <sameerauf1@gmail.com>
2024-03-02 11:33:40 -0800

    created samosa counter

---
+++ Samosa/state-stuff/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  function handleCount(){
+    setCount(count + 1)
+  }
+
+  return (
+    <>
+      <div>
+       <h1> Count: {count}</h1>
+       <button onClick = {handleCount}> Set Count</button>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ samosa-lab/samosa-selector/src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './App.css';
+import samosaImage from './assets/samosa.jpeg';
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
+
+  const updateCount = () => setCount(count + multiplier);
+
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Samosa Selector</h1>
+        <h2>Count: {count}</h2>
+        <img src={samosaImage} className="samosa" alt="Samosa" onClick={updateCount} />
+      </div>
+      <div className="container">
+        <div className="upgrade">
+          <h3>Double Stuffed 👯‍♀️</h3>
+          <p>2x per click</p>
+          <button className="button">10 samosas</button>
+        </div>
+        <div className="upgrade">
+          <h3>Party Pack 🎉</h3>
+          <p>5x per click</p>
+          <button className="button">100 samosas</button>
+        </div>
+        <div className="upgrade">
+          <h3>Full Feast 👩🏽‍🍳</h3>
+          <p>10x per click</p>
+          <button className="button">1000 samosas</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
